0219672c08d78dc272e8dbcd6f78d9118b175df1
Breno Narde Silva <brenonarde@gmail.com>
2023-11-30 14:30:24 -0300

    ajusta relogio pomodoro e adiciona botao de restart

--- src/app/components/pomodoro/pomodoro.component.ts
+++ src/app/components/pomodoro/pomodoro.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-pomodoro',
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrl: './pomodoro.component.css'
 })
 export class PomodoroComponent {
-  pomodoroTime: number = 25.00;
+  @ViewChild('btnIniciar') btnIniciar!: ElementRef
+
+  minutos: number = 25;
+  segundos: number = 0;
+  intervalo: any
+  rodando: boolean = false;
+
+  iniciar() {
+    this.intervalo = setInterval(() => {
+      this.rodando = true;
+      if (this.segundos > 0) {
+        this.segundos--;
+      }
+      else if (this.minutos > 0) {
+        this.segundos = 59;
+        this.minutos--;
+      }
+      else {
+        clearInterval(this.intervalo);
+        this.rodando = false;
+      }
+    }, 1000);
+  }
+
+  pausar() {
+    clearInterval(this.intervalo);
+    this.rodando = false;
+  }
+
+  reiniciar() {
+    this.minutos = 25;
+    this.segundos = 0;
+    clearInterval(this.intervalo);
+    this.rodando = false;
+  }
 }
